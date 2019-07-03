920990f7c80917abb57bce53ec2b6fe27df16802
jqim87 <jqim87@gmail.com>
2019-07-03 15:55:19 +0100

    file name output as same as source caused th @imports not to work

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,35 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-  entry: ['./src/app.js', './src/main.scss'],
+  entry: ['./src/app.js','./src/index.scss'],
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'app.js'
   },
+  mode: 'production',
   module: {
       rules: [{
               test: /\.scss$/,
-              use: [{
-                loader: "file-loader",
-                options: {
-                    name: "[name].css",
-                    }
-                },
-                { loader:'extract-loader' },
+              use: [
+                // {
+                //     loader: "file-loader",
+                //     options: {
+                //         name: "[name].css",
+                //         }
+                // },
+                // { loader:'extract-loader' },
+                { loader: MiniCssExtractPlugin.loader},
                 { loader:'css-loader' },
-                { loader:'sass-loader'}
+                { loader:'sass-loader' }
             ]},
           
       ]},
   plugins: [new HtmlWebpackPlugin({
     template: 'src/index.html',
     title: 'Utility spacing',
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'main.css'
   })]
 };
